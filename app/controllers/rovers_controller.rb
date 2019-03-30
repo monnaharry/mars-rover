5e585b6b03fde04458bb0827114d0b47a9650633
Rebecca Green <rebeccalynngreen@gmail.com>
2019-03-30 15:56:11 +0100

    added move left and right methods

---
+++ app/controllers/rovers_controller.rb
@@ -0,0 +1,9 @@
+class RoversController
+  def initialize(input, plateau)
+    @input = input
+    @plateau = plateau
+    @view = RoverView.new
+  end
+
+  def
+end
--- app/models/rover.rb
+++ app/models/rover.rb
@@ -1,21 +1,45 @@
 class Rover
+  attr_accessor :x, :y, :plateau, :orientation
   def initialize(attributes = {})
-    @x_start = attributes[:x_start]
-    @y_start = attributes[:y_start]
+    @x = attributes[:x]
+    @y = attributes[:y]
     @plateau = attributes[:plateau]
     @orientation = attributes[:orientation]
   end
 
   def left
+    case @orientation
+    when 'N' then @orientation = 'W'
+    when 'E' then @orientation = 'N'
+    when 'S' then @orientation = 'E'
+    when 'W' then @orientation = 'S'
+    end
   end
 
   def right
+    case @orientation
+    when 'N' then @orientation = 'E'
+    when 'E' then @orientation = 'S'
+    when 'S' then @orientation = 'W'
+    when 'W' then @orientation = 'N'
+    end
   end
 
   def move
-  end
-
-  def self.direction
-
+    case @orientation
+    when 'N' then @y += 1
+    when 'E' then @x += 1
+    when 'S' then @y -= 1
+    when 'W' then @x -= 1
+    end
   end
 end
+
+# r = Rover.new(orientation: 'E', x: 0, y: 0)
+
+# r.right
+# p r.orientation
+
+# p r
+# r.move
+# p r
