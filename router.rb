b1441b95b9bf0767a7dfcd6fcef2347c3439108a
Rebecca Green <rebeccalynngreen@gmail.com>
2019-04-07 22:03:26 +0200

    removed methods from router

--- router.rb
+++ router.rb
@@ -1,33 +1,6 @@
 class Router
   def initialize
-    @running = true
     @rovers_controller = RoversController.new
-  end
-
-  def run
-    while @running
-      display_tasks
-      action = gets.chomp.to_i
-      route_action(action)
-    end
-  end
-
-  def stop
-    @running = false
-  end
-
-  def display_tasks
-    puts ''
-    puts '1 - Start'
-    puts '2 - Quit'
-    puts ''
-  end
-
-  def route_action(action)
-    case action
-    when 1 then start_program
-    when 2 then stop
-    end
   end
 
   def start_program
